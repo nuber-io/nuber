52dc18a9b0fd47b0517ba0ed649e077ba010a78e
Jamiel Sharief <20553479+jamielsharief@users.noreply.github.com>
2021-06-22 09:43:32 +0200

    Added copyright header

--- app/Console/Command/ImageListCommand.php
+++ app/Console/Command/ImageListCommand.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Nuber.io
+ * Copyright 2020 - 2021 Jamiel Sharief.
+ *
+ * SPDX-License-Identifier: AGPL-3.0
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.nuber.io
+ * @license     https://opensource.org/licenses/AGPL-3.0 AGPL-3.0 License
+ */
 declare(strict_types = 1);
 namespace App\Console\Command;
 
