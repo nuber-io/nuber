402ea0b7670b323ab99185d916ea1bf60ec7fb00
Jamiel Sharief <20553479+jamielsharief@users.noreply.github.com>
2021-08-11 11:22:37 +0200

    Fixed incorrect operator

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@
  */
 if (php_sapi_name() === 'cli-server') {
     $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    if (file_exists(__DIR__ . $path) and is_file(__DIR__ . $path)) {
+    if (file_exists(__DIR__ . $path) && is_file(__DIR__ . $path)) {
         return false;
     }
 }
